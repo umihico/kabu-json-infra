c581041d2a4b12608f36399a4802b8b9a69acb23
umihico <umihico@users.noreply.github.com>
2024-09-14 00:58:42 +0900

    feat: 公開データ配信用のS3, Cloudfrontを設定

---
+++ README.md
@@ -0,0 +1,5 @@
+# 【株JSON】 データ保存・配信用インフラ
+
+AWS x Terraform
+
+暫定URL: https://d1rrtoo3h22gy6.cloudfront.net/
---
+++ public/main.tf
@@ -0,0 +1,107 @@
+terraform {
+  backend "s3" {
+    bucket         = "kabu-json-terraform-states"
+    key            = "public/terraform.tfstate"
+    dynamodb_table = "kabu-json-terraform-states-locker"
+    region         = "ap-northeast-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "kabu-json-public-static-data-bucket"
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_object" "hello_world" {
+  bucket       = aws_s3_bucket.website_bucket.id
+  key          = "index.html"
+  content_type = "text/html"
+  content      = <<-EOF
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Hello, World!</title>
+      </head>
+      <body>
+        <h1>Hello, World!</h1>
+      </body>
+    </html>
+  EOF
+}
+
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "OAI for accessing S3 bucket"
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${aws_cloudfront_origin_access_identity.oai.id}"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_cloudfront_distribution" "website_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.website_bucket.bucket
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.website_bucket.bucket
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.website_distribution.domain_name
+}
