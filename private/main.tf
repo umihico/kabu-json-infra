14ac0c22ed8d22962f320f45dd85ec3dab2b3932
umihico <umihico@users.noreply.github.com>
2024-11-14 00:05:15 +0900

    feat: プライベートなバケットの方もbasic認証付けてWEB公開

--- private/main.tf
+++ private/main.tf
@@ -25,3 +25,125 @@ resource "aws_s3_bucket_public_access_block" "public_access_block" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_object" "hello_world" {
+  bucket       = aws_s3_bucket.private_bucket.id
+  key          = "index.html"
+  content_type = "text/html"
+  content      = <<-EOF
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Hello, World!</title>
+      </head>
+      <body>
+        <h1>Hello, World!</h1>
+      </body>
+    </html>
+  EOF
+}
+
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "OAI for accessing S3 bucket"
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.private_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${aws_cloudfront_origin_access_identity.oai.id}"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.private_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_cloudfront_distribution" "website_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.private_bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.private_bucket.bucket
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.private_bucket.bucket
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    function_association {
+      event_type   = "viewer-request"
+      function_arn = aws_cloudfront_function.basic_auth.arn
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+variable "private_cloudfront_password" {
+  type = string
+  # environment variable TF_VAR_private_cloudfront_password
+}
+
+locals {
+  basic_auth = base64encode("umihico:${var.private_cloudfront_password}")
+  # echo -n umihico:password1234 | base64
+}
+
+resource "aws_cloudfront_function" "basic_auth" {
+  name    = "basic-auth"
+  runtime = "cloudfront-js-1.0"
+  comment = "https://dev.classmethod.jp/articles/apply-basic-authentication-password-with-cloudfront-functions/"
+  publish = true
+  code    = <<CODE
+function handler(event) {
+  var request = event.request;
+  var headers = request.headers;
+
+  var authString = "Basic ${local.basic_auth}";
+
+  if (typeof headers.authorization === "undefined" || headers.authorization.value !== authString) {
+    return {
+      statusCode: 401,
+      statusDescription: "Unauthorized",
+      headers: { "www-authenticate": { value: "Basic" } }
+    };
+  }
+
+  return request;
+}
+CODE
+}
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.website_distribution.domain_name
+}
