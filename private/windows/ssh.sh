d03dea3823b8ddc4ad905dff2e0ab33c323b42c4
umihico <umihico@users.noreply.github.com>
2024-11-19 00:50:54 +0900

    feat: 不要なポートフォワーディングを除去

--- private/windows/ssh.sh
+++ private/windows/ssh.sh
@@ -9,7 +9,6 @@ echo "  HostName ${ip}" >> ~/.ssh/config.d/kabu-json-windows
 echo "  RequestTTY no" >> ~/.ssh/config.d/kabu-json-windows
 echo "  User Administrator" >> ~/.ssh/config.d/kabu-json-windows
 echo "  IdentityFile ~/.ssh/kabu-json-windows.pem" >> ~/.ssh/config.d/kabu-json-windows
-echo "  LocalForward 3389 localhost:3389" >> ~/.ssh/config.d/kabu-json-windows
 echo "  LocalForward 18080 localhost:18080" >> ~/.ssh/config.d/kabu-json-windows
 echo "  LocalForward 18081 localhost:18081" >> ~/.ssh/config.d/kabu-json-windows
 
