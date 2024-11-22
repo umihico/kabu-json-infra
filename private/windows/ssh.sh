f7d40e51eac7cad52ca283d3a11f8b5fc39f4884
umihico <umihico@users.noreply.github.com>
2024-11-23 00:16:41 +0900

    feat: 落ちないように

--- private/windows/ssh.sh
+++ private/windows/ssh.sh
@@ -11,5 +11,7 @@ echo "  User Administrator" >> ~/.ssh/config.d/kabu-json-windows
 echo "  IdentityFile ~/.ssh/kabu-json-windows.pem" >> ~/.ssh/config.d/kabu-json-windows
 echo "  LocalForward 18080 localhost:18080" >> ~/.ssh/config.d/kabu-json-windows
 echo "  LocalForward 18081 localhost:18081" >> ~/.ssh/config.d/kabu-json-windows
+echo "  ServerAliveInterval 60" >> ~/.ssh/config.d/kabu-json-windows
+echo "  ServerAliveCountMax 0" >> ~/.ssh/config.d/kabu-json-windows
 
 ssh kabu-json-windows
