74984c71803e48477153a9a401b0f5c971f4e94f
umihico <umihico@users.noreply.github.com>
2024-11-27 23:23:18 +0900

    feat: setup.shでインスタンス立ち上げも行う

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euoxv pipefail
 
+terraform -chdir=private apply -auto-approve -var=instance_names=0
+sleep 30 # インスタンスが立ち上がるまで待つ
 sh private/kabustation/ssh-windows.sh pwd
 sh private/kabustation/ssh-linux.sh tmux new -d -s winssh ssh kabu-json-windows
 sh private/kabustation/rdp.sh
