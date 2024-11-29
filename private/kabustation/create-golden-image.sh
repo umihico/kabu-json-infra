e3be4e768815160b8814e268492ace5b8181f66a
umihico <umihico@users.noreply.github.com>
2024-11-29 23:27:24 +0900

    feat: 待機、AMIバックアップ

--- private/kabustation/create-golden-image.sh
+++ private/kabustation/create-golden-image.sh
@@ -7,6 +7,10 @@ echo $WINDOWS_INSTANCE_ID
 echo $LINUX_INSTANCE_ID
 aws ec2 stop-instances --instance-ids ${WINDOWS_INSTANCE_ID}
 aws ec2 stop-instances --instance-ids ${LINUX_INSTANCE_ID}
+aws ec2 deregister-image --image-id $(aws ec2 describe-images --filters "Name=name,Values=kabu-json-windows-backup" --query 'Images[*].ImageId' --output text)
+aws ec2 deregister-image --image-id $(aws ec2 describe-images --filters "Name=name,Values=kabu-json-linux-backup" --query 'Images[*].ImageId' --output text)
+aws ec2 copy-image --name kabu-json-windows-backup --source-image-id $(aws ec2 describe-images --filters "Name=name,Values=kabu-json-windows" --query 'Images[*].ImageId' --output text) --source-region ap-northeast-1
+aws ec2 copy-image --name kabu-json-linux-backup --source-image-id $(aws ec2 describe-images --filters "Name=name,Values=kabu-json-linux" --query 'Images[*].ImageId' --output text) --source-region ap-northeast-1
 aws ec2 wait instance-stopped --instance-ids ${WINDOWS_INSTANCE_ID}
 aws ec2 wait instance-stopped --instance-ids ${LINUX_INSTANCE_ID}
 aws ec2 deregister-image --image-id $(aws ec2 describe-images --filters "Name=name,Values=kabu-json-windows" --query 'Images[*].ImageId' --output text)
--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 set -euoxv pipefail
 
 terraform -chdir=private apply -auto-approve -var=instance_names=0
-sleep 30 # インスタンスが立ち上がるまで待つ
+WINDOWS_INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=kabu-json-windows" "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].InstanceId' --output text)
+LINUX_INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=kabu-json-linux" "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].InstanceId' --output text)
+aws ec2 wait instance-status-ok --instance-ids ${WINDOWS_INSTANCE_ID} ${LINUX_INSTANCE_ID}
 sh private/kabustation/ssh-windows.sh pwd
 sh private/kabustation/ssh-linux.sh tmux new -d -s winssh ssh kabu-json-windows
-sh private/kabustation/rdp.sh
-sh private/kabustation/ssh-linux.sh
