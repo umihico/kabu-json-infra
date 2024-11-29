2c21819b260b8916fd085eef1be36521c3070682
umihico <umihico@users.noreply.github.com>
2024-11-29 23:26:07 +0900

    feat: ログインスクリプト改善

--- private/kabustation/login.py
+++ private/kabustation/login.py
@@ -4,13 +4,23 @@
 import pyautogui
 import time
 
+pyautogui.FAILSAFE = False  # PyAutoGUI fail-safe triggered from mouse moving to a corner of the screen. To disable this fail-safe, set pyautogui.FAILSAFE to False. DISABLING FAIL-SAFE IS NOT RECOMMENDED. 防止
+
+# たまに何故かエンター押しても入れないことあるので、exitせずログイン窓がある限り1000秒間試行する
 for attempt in range(0, 1000):
-    print(f"Attempt {attempt}")
-    apps = pyautogui.getWindowsWithTitle("ログイン")
-    if len(apps) == 0:
+    try:
         time.sleep(1)
-        continue
-    apps[0].activate()
-    time.sleep(3)
-    pyautogui.press("enter")
-    break
+        print(f"試行{attempt}")
+        apps = pyautogui.getWindowsWithTitle("ログイン")
+        if len(apps) == 0:
+            print("ログインが見つからない")
+            continue
+        apps[0].activate()
+        time.sleep(1)
+        pyautogui.press("enter")
+        print("エンター押下")
+    except Exception as e:
+        print(e)
+
+# 実行ログを残すようにプロンプトを出して起動しっぱなしにする
+input()
