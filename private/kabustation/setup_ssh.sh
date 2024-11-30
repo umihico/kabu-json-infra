923a59e2d18c00fd99d345de27df33766ab5f4af
umihico <umihico@users.noreply.github.com>
2024-11-30 16:48:45 +0900

    feat: SSHの設定コマンドを集約・SSHまで実行しないように

--- private/kabustation/setup_ssh.sh
+++ private/kabustation/setup_ssh.sh
@@ -17,7 +17,30 @@ echo "  LocalForward 18081 localhost:18081" >> ~/.ssh/config.d/kabu-json-windows
 echo "  ServerAliveInterval 10" >> ~/.ssh/config.d/kabu-json-windows
 echo "  ServerAliveCountMax 10" >> ~/.ssh/config.d/kabu-json-windows
 
-ssh kabu-json-windows $@
+ip=$(cat private/kabustation/linux_ip.json)
+windows_hostname=$(cat private/kabustation/windows_hostname.json)
+echo "Host kabu-json-linux" > ~/.ssh/config.d/kabu-json-linux
+echo "  HostName ${ip}" >> ~/.ssh/config.d/kabu-json-linux
+echo "  User ec2-user" >> ~/.ssh/config.d/kabu-json-linux
+echo "  IdentityFile ~/.ssh/kabu-json-kabustation.pem" >> ~/.ssh/config.d/kabu-json-linux
+echo "  RequestTTY yes" >> ~/.ssh/config.d/kabu-json-linux # forceにするとrsyncがコケるようになった: protocol version mismatch -- is your shell clean? rsync error: protocol incompatibility (code 2) at compat.c(626) [sender=3.3.0]
+echo "  StrictHostKeyChecking no" >> ~/.ssh/config.d/kabu-json-linux
+echo "  ServerAliveInterval 10" >> ~/.ssh/config.d/kabu-json-linux
+echo "  ServerAliveCountMax 10" >> ~/.ssh/config.d/kabu-json-linux
+echo "  LocalForward 3389 localhost:3389" >> ~/.ssh/config.d/kabu-json-linux
+
+# イメージがリセットされたら、秘密鍵を再度設定する必要がある
+# 多段SSHはProxyJumpの方が好ましいが、ローカル端末抜きでLinuxがWindowsに常時接続、売買を実行してほしいので、秘密鍵含め各クレデンシャルをWindowsに持たせる
+# echo "KABU_STATION_API_PROD_PASSWORD=${KABU_STATION_API_PROD_PASSWORD}" > private/kabustation/.env.linux
+# echo "KABU_COM_PASSWORD=${KABU_COM_PASSWORD}" >> private/kabustation/.env.linux
+# echo "WINDOWS_HOSTNAME=${windows_hostname}" >> private/kabustation/.env.linux
+# scp private/kabustation/kabu-json-kabustation.pem kabu-json-linux:~/.ssh/
+# ssh kabu-json-linux 'chmod 600 ~/.ssh/kabu-json-kabustation.pem;'
+# scp private/kabustation/.env.linux kabu-json-linux:~/.env.linux
+# scp ~/.ssh/config.d/kabu-json-windows kabu-json-linux:~/.ssh/config
+
+
+
 # 公開鍵を変更してしまうと、以下のエラーがでるため、ユーザーデータを参考に再度公開鍵を設定し、ゴールデンイメージにして固める
 # Permission denied (publickey,keyboard-interactive)
 # 以下のコマンドをRDPしてPowerShellで実行する
