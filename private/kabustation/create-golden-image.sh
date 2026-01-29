fc2d54a6984b9934e59d7c6be952de82112dcfc0
umihico <umihico@users.noreply.github.com>
2026-01-29 15:28:36 +0900

    fix: ゴールデンイメージ作成時にインスタンス停止を確実に実行し、AMIの整合性を担保

--- private/kabustation/create-golden-image.sh
+++ private/kabustation/create-golden-image.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -euoxv pipefail
 
-WINDOWS_INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=kabu-json-windows" "Name=instance-state-name,Values=running,stopped" --query 'Reservations[*].Instances[*].InstanceId' --output text)
-LINUX_INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=kabu-json-linux" "Name=instance-state-name,Values=running,stopped" --query 'Reservations[*].Instances[*].InstanceId' --output text)
-echo $WINDOWS_INSTANCE_ID
-echo $LINUX_INSTANCE_ID
-# aws ec2 stop-instances --instance-ids ${WINDOWS_INSTANCE_ID} ${LINUX_INSTANCE_ID}
+echo "実行中のインスタンスを検索中..."
+WINDOWS_INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=kabu-json-windows" "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].InstanceId' --output text)
+LINUX_INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=kabu-json-linux" "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].InstanceId' --output text)
+echo "WindowsインスタンスID: ${WINDOWS_INSTANCE_ID}"
+echo "LinuxインスタンスID: ${LINUX_INSTANCE_ID}"
+aws ec2 stop-instances --instance-ids ${WINDOWS_INSTANCE_ID} ${LINUX_INSTANCE_ID}
+sleep 10
+aws ec2 wait instance-stopped --instance-ids ${WINDOWS_INSTANCE_ID} ${LINUX_INSTANCE_ID}
 aws ec2 deregister-image --image-id $(aws ec2 describe-images --filters "Name=name,Values=kabu-json-windows-backup" --query 'Images[*].ImageId' --output text)
 aws ec2 deregister-image --image-id $(aws ec2 describe-images --filters "Name=name,Values=kabu-json-linux-backup" --query 'Images[*].ImageId' --output text)
 aws ec2 copy-image --name kabu-json-windows-backup --source-image-id $(aws ec2 describe-images --filters "Name=name,Values=kabu-json-windows" --query 'Images[*].ImageId' --output text) --source-region ap-northeast-1
 aws ec2 copy-image --name kabu-json-linux-backup --source-image-id $(aws ec2 describe-images --filters "Name=name,Values=kabu-json-linux" --query 'Images[*].ImageId' --output text) --source-region ap-northeast-1
-# aws ec2 wait instance-stopped --instance-ids ${WINDOWS_INSTANCE_ID} ${LINUX_INSTANCE_ID}
 aws ec2 deregister-image --image-id $(aws ec2 describe-images --filters "Name=name,Values=kabu-json-windows" --query 'Images[*].ImageId' --output text)
 aws ec2 deregister-image --image-id $(aws ec2 describe-images --filters "Name=name,Values=kabu-json-linux" --query 'Images[*].ImageId' --output text)
 aws ec2 create-image --instance-id ${WINDOWS_INSTANCE_ID} --name "kabu-json-windows" --reboot
